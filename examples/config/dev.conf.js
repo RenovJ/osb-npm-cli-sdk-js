376158042e91a87f136bf0d1817a8d9466552707
Renov <renov.jung@gmail.com>
2019-12-26 17:25:16 +0900

    configure file for example

---
+++ examples/config/dev.conf.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'CHAIN_PROTOCOL' : 'http',
+    'CHAIN_IPADDR' : '18.141.2.126',
+    'CHAIN_PORT' : '13876',
+    'CHAIN_ID' : '2cfbbc1ce6f7c4a48cfbb6de6ad00c55ea21401fbcf983a6f553fb4d8923d695',
+    'DATA_TRADE_CONTRACT_NAME' : 'datatrader13',
+    'ACCOUNT' : '',
+    'ACCOUNT_PRIVATE_KEY' : '',
+    'BUYER_PRIVATE_KEY' : '',
+    'PUBKEY_PREFIX' : 'OSB'
+}
